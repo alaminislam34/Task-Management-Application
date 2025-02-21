15a3ce007a4ee9a29635469680b047d954ec18e3
alaminislam34 <mi3548514@gmail.com>
2025-02-21 08:22:36 +0600

    added shared component file and add footer component

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center py-6 lg:py-8 mt-6 bg-base-300 px-2">
+      <p className="text-xs text-center">
+        Copyright &copy; 2023 All Rights Reserved by Task Management
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
